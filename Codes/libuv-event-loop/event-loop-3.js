27f08d9e10b01937cfa32dea9c8a24c9f45b3990
Shankha Shubhra Das <ribhumaster@gmail.com>
2024-11-09 21:26:59 +0530

    libuv and event loop done

---
+++ Codes/libuv-event-loop/event-loop-3.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+setImmediate(() => console.log("setImmediate"));
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", () => {
+  setTimeout(() => console.log("2nd timer"), 0);
+
+  process.nextTick(() => console.log("2nd nextTick"));
+
+  setImmediate(() => console.log("2nd setImmediate"));
+
+  console.log("File Reading CB");
+});
+
+process.nextTick(() => console.log("process.nextTick"));
+
+console.log("Last Line of the file");
